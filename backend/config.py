781965b139dceb89950f07ed4141138674ac15c1
Evan Hoyt <evanhoyt10@icloud.com>
2026-05-20 21:59:41 -0400

    Updated NOAA HAB url

--- backend/config.py
+++ backend/config.py
@@ -3,6 +3,6 @@ import os
 
 load_dotenv()
 
-NOAA_HAB_BASE_URL = "https://habsos.noaa.gov/api/"
+NOAA_HAB_BASE_URL = "https://gis.ngdc.noaa.gov/arcgis/rest/services/EnvironmentalMonitoring/HABSOSViewBase/MapServer/0/query"
 INATURALIST_BASE_URL = "https://api.inaturalist.org/v1"
 NOAA_TIDES_BASE_URL = "https://api.tidesandcurrents.noaa.gov/api/prod/datagetter"
